02297435832d1461f3c81d0359605742f3a95d89
Omar <omarvx211@gmail.com>
2026-07-25 08:46:42 +0000

    feat(ui): auto-pair brackets/quotes via mini.pairs

--- init.lua
+++ init.lua
@@ -1243,6 +1243,9 @@ require('lazy').setup({
         },
       })
 
+      -- Auto-pair brackets, parens, quotes: when you type ( it adds ), etc.
+      require('mini.pairs').setup {}
+
       -- Statusline is handled by lualine.nvim (see LUALINE plugin entry below).
 
       -- ... and there is more!
